bb5e92a0d730f1a761c0b96373186a29b17e36e1
Daniel Truong <dtruong@netapp.com>
2017-08-26 18:52:29 -0700

    Add i18N config, service and messages

--- src/test/java/com/devops/DevopsApplicationTests.java
+++ src/test/java/com/devops/DevopsApplicationTests.java
@@ -1,16 +1,30 @@
 package com.devops;
 
+import com.devops.web.i18n.I18NService;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = DevopsApplication.class)
+@WebAppConfiguration
 public class DevopsApplicationTests {
 
+	@Autowired
+	private I18NService i18NService;
+
 	@Test
-	public void contextLoads() {
+	public void testMessageByLocaleService() throws Exception {
+		String expectedResult = "Bootstrap starter template";
+		String messageId = "index.main.callout";
+
+		String actual = i18NService.getMessage(messageId);
+		Assert.assertEquals("The actuall and expected string don't match", expectedResult, actual);
 	}
 
 }
