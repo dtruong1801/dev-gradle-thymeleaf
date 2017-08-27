2099a7fbab8659f1a8164b03b1afde55176c6b15
Daniel Truong <dtruong@netapp.com>
2017-08-26 19:08:09 -0700

    Add logging: logback

--- src/main/java/com/devops/web/i18n/I18NService.java
+++ src/main/java/com/devops/web/i18n/I18NService.java
@@ -1,5 +1,7 @@
 package com.devops.web.i18n;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -13,6 +15,9 @@ import java.util.Locale;
 @Service
 public class I18NService {
 
+    /** the application logger */
+    private static final Logger LOG = LoggerFactory.getLogger(I18NService.class);
+
     @Autowired
     private MessageSource messageSource;
 
@@ -21,10 +26,17 @@ public class I18NService {
      * @param messageId The key to the message resource file
      */
     public String getMessage(String messageId) {
+        LOG.info("Returning i18n for messageId {}", messageId);
         Locale locale = LocaleContextHolder.getLocale();
         return getMessage(messageId, locale);
     }
 
+    /**
+     * Return a message for the given message id and locale
+     * @param messageId The key to the messages resource file
+     * @param locale The locale
+     * @return
+     */
     public String getMessage(String messageId, Locale locale) {
         return messageSource.getMessage(messageId, null, locale);
     }
